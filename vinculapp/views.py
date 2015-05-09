f9a4b5db529bd0cf0923beb62713448e14511bad
Albertitou26 <arincon1992@gmail.com>
2015-05-09 13:07:13 +0200

    login template, refactorizando master

--- vinculapp/views.py
+++ vinculapp/views.py
@@ -4,6 +4,9 @@ from .models import *
 
 # Create your views here.
 
+def login(request):
+	return render(request, 'vinculapp/login.html', locals())
+
 def index(request):
 	
 	return render(request, 'vinculapp/master.html', locals())
