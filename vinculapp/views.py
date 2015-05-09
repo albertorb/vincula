96a574334619a1627c862914253ff30dc5016ed1
Albertitou26 <arincon1992@gmail.com>
2015-05-09 13:45:13 +0200

    login view y urls redirect

--- vinculapp/views.py
+++ vinculapp/views.py
@@ -1,10 +1,24 @@
+# encoding: utf8
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate
 from .models import *
 
 # Create your views here.
 
 def login(request):
+	errors = False
+	if request.POST:
+		user = authenticate(username = request.POST['username'], password = request.POST['password'])
+		if user is not None:
+			if user.is_active:
+				return HttpResponseRedirect('/home')
+			else:
+				return HttpResponse("Esta cuenta de usuario aún no ha sido activada")
+		else:
+			errors = True
+			return render(request, 'vinculapp/login.html', locals())
+
 	return render(request, 'vinculapp/login.html', locals())
 
 def index(request):
