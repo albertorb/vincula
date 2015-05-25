37bcb6f41e31685eeb50dfca4332501c1a29392e
Albertitou26 <arincon1992@gmail.com>
2015-05-25 20:03:17 +0200

    card images and media files

--- vinculapp/miscellanea.py
+++ vinculapp/miscellanea.py
@@ -4,23 +4,29 @@ import os
 
 def create_vin(request):
 	directory = "media/series"
-	root = Folder.create('ULTIMO',request.user.profile,None)
+	root = Folder.create('SERIES',request.user.profile,None,'example_folder.png')
 	root.save()
 	for serie in os.listdir(directory):
 		if not serie.startswith('.'):
-			s = Folder.create(serie,request.user.profile,root)
-			s.save()
+			s = Folder.create(serie,request.user.profile,root,None)
+			s.pic = None #TODO revisar codigo inutil
 			for season in os.listdir(directory+'/'+serie):
 				if not season.startswith('.'):
-					ss = Folder.create(season,request.user.profile,s)
-					ss.pic='tv.png'
+					pic = '/'+directory+'/'+serie+'/'+season+'/season.jpeg'
+					if s.pic == None:
+						s.pic = pic
+						s.save()
+					ss = Folder.create(season,request.user.profile,s,pic)
 					ss.save()
 					for episode in os.listdir(directory+'/'+serie+'/'+season+'/'):
-						if not episode.startswith('.'):
+						if not episode.startswith('.') and episode != 'season.jpeg':
 							url = open(directory+'/'+serie+'/'+season+'/'+episode,'r').readline()
 							if url.__len__ > 0:
-								c = Card.create(episode,url,ss)
+								c = Card.create(episode,url,ss)							
 								c.save()
+
+def slugfy(string):
+	return string.replace(" ", "-")
 
 
 
--- vinculapp/models.py
+++ vinculapp/models.py
@@ -1,3 +1,4 @@
+#encoding:utf-8
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,20 +6,20 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 	user = models.OneToOneField(User)
-	pic = models.ImageField(upload_to = 'user-pic/', default = '/static/vinculapp/img/example_profile.png')
+	pic = models.ImageField(upload_to = 'user-pic/', default = '/static/vinculapp/img/example_profile.png', max_length=255)
 
 	def __unicode__(self):
 		return self.user.get_username()
 
 class Folder(models.Model):
 	profile = models.ForeignKey(Profile, default = None)
-	name = models.TextField(max_length=100)
+	name = models.TextField(max_length=255)
 	parent = models.ForeignKey('self', null = True, blank = True, default = None)
-	pic = models.ImageField(upload_to = 'folders-pic/', default = 'example_folder.png', blank = True, null = True)
+	pic = models.ImageField(upload_to = 'folders-pic/', default = 'example_folder.png', blank = True, null = True, max_length=255)
 
 	@classmethod
-	def create(cls,name,profile,parent):
-		folder = cls(name=name,profile=profile,parent=parent)
+	def create(cls,name,profile,parent,pic):
+		folder = cls(name=name,profile=profile,parent=parent, pic=pic)
 		return folder
 
 	def __unicode__(self):
@@ -27,9 +28,9 @@ class Folder(models.Model):
 
 
 class Card(models.Model):
-	name = models.TextField(max_length=100)
-	url = models.TextField()
-	pic = models.ImageField(upload_to = 'cards-pic/', blank=True, null=True, default = 'video.jpeg')
+	name = models.TextField(max_length=255)
+	url = models.TextField(max_length=255)
+	pic = models.ImageField(upload_to = 'cards-pic/', blank=True, null=True, default = 'video.jpeg', max_length=255)
 	folder = models.ForeignKey(Folder)
 
 	@classmethod
