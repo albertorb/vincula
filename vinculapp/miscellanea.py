e4de229169cd3103a90b037b9dd4b57dac120779
Alberto Rincón Borreguero <albrinbor@MacBook-Air-de-Alberto.local>
2016-01-13 20:30:16 +0100

    some minor fixes

--- vinculapp/miscellanea.py
+++ vinculapp/miscellanea.py
@@ -3,29 +3,29 @@ from .models import Folder,Card
 import os
 
 def create_vin(request):
-	topics = ['series', 'NBA']
-	for fld in topics:
-		directory = "media/%s" % fld
-		root = Folder.create(fld,request.user.profile,None,'/media/example_folder.png')
-		root.save()
-		for serie in os.listdir(directory):
-			if not serie.startswith('.'):
-				s = Folder.create(serie,request.user.profile,root,None)
-				s.pic = None #TODO revisar codigo inutil
-				for season in os.listdir(directory+'/'+serie):
-					if not season.startswith('.'):
-						pic = '/'+directory+'/'+serie+'/'+season+'/season.jpeg'
-						if s.pic == None:
-							s.pic = pic
-							s.save()
-						ss = Folder.create(season,request.user.profile,s,pic)
-						ss.save()
-						for episode in os.listdir(directory+'/'+serie+'/'+season+'/'):
-							if not episode.startswith('.') and episode != 'season.jpeg':
-								url = open(directory+'/'+serie+'/'+season+'/'+episode,'r').readline()
-								if url.__len__ > 0:
-									c = Card.create(episode,url,ss)
-									c.save()
+	#topics = ['series', 'NBA']
+	#for fld in topics:
+	directory = "media/NBA" #"media/%s" % fld
+	root = Folder.create(fld,request.user.profile,None,'/media/example_folder.png')
+	root.save()
+	for serie in os.listdir(directory):
+		if not serie.startswith('.'):
+			s = Folder.create(serie,request.user.profile,root,None)
+			s.pic = None #TODO revisar codigo inutil
+			for season in os.listdir(directory+'/'+serie):
+				if not season.startswith('.'):
+					pic = '/'+directory+'/'+serie+'/'+season+'/season.jpeg'
+					if s.pic == None:
+						s.pic = pic
+						s.save()
+					ss = Folder.create(season,request.user.profile,s,pic)
+					ss.save()
+					for episode in os.listdir(directory+'/'+serie+'/'+season+'/'):
+						if not episode.startswith('.') and episode != 'season.jpeg':
+							url = open(directory+'/'+serie+'/'+season+'/'+episode,'r').readline()
+							if url.__len__ > 0:
+								c = Card.create(episode,url,ss)
+								c.save()
 
 def slugfy(string):
 	return string.replace(" ", "-")
